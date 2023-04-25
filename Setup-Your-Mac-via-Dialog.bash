b7374193aab86285e43d790d10d7c9fc84344bbe
Andrew Clark <drtaru@gmail.com>
2023-04-25 10:55:43 -0400

    Revert "Remote Validation Additions"
    This reverts commit 9611f1bca489ae32a7df03df8e07916eb7ebea34.

--- Setup-Your-Mac-via-Dialog.bash
+++ Setup-Your-Mac-via-Dialog.bash
@@ -1908,8 +1908,6 @@ function validatePolicyResult() {
                 result=$( "${jamfBinary}" policy -trigger "${trigger}" | grep "Script result:" )
                 if [[ "${result}" == *"Running"* ]]; then
                     dialogUpdateSetupYourMac "listitem: index: $i, status: success, statustext: Running"
-                elif [[ "${result}" == *"Installed"* || "${result}" == *"Success"*  ]]; then
-                    dialogUpdateSetupYourMac "listitem: index: $i, status: success, statustext: Installed"
                 else
                     dialogUpdateSetupYourMac "listitem: index: $i, status: fail, statustext: Failed"
                     jamfProPolicyTriggerFailure="failed"
