0c5aa730d976e7716fc65a2710c2c3c1b23935eb
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2025-12-29 10:11:14 -0500

    0.0.4
    Signed-off-by: Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>

--- Validations/Microsoft Office 365.bash
+++ Validations/Microsoft Office 365.bash
@@ -12,7 +12,7 @@
 #
 #   Version 0.0.3, 25-Apr-2023, Andrew Clark (@drtaru)
 #   - Changed Success result to Success to map to new SYM validation status
-#   Version 0.0.4 - 13-Jun-2024, Lewis B (@master-vodawagner)
+#   Version 0.0.4, 13-Jun-2024, Lewis B (@master-vodawagner)
 #   - Added ability to delay whilst Microsoft AutoUpdater is running/patching apps
 #
 ####################################################################################
@@ -74,7 +74,7 @@ function appCheck() {
 # Check for Microsoft Office 365 apps (i.e., Microsoft_365_and_Office_16.70.23021201_Installer.pkg)
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-#Allow Microsoft AutoUpdater to scan/patch apps before validating install
+# Allow Microsoft AutoUpdater to scan/patch apps before validating install
 while pgrep -a "Microsoft AutoUpdate"; do
     echo "Microsoft AutoUpdater is still running; pausing for 10 seconds"
     sleep 10
